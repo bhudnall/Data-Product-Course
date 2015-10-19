be0db94ce7ce948c19346e94fc5d7c06f93cebd0
bhudnall <brian.hudnall@gmail.com>
2015-10-18 20:58:12 -0700

    First Commit

---
+++ DataProductCourse.r
@@ -0,0 +1,13 @@
+## Idea for Project:
+# Create shiny app that allows user to upload a file in either wide or long format
+# After file is uploaded the app asks users to pick variables to include in analysis
+# Must indicate whether the variables are the independent or dependent variables
+# also indicate if the independant variables are factor variables
+# from there do a visualization of the independent variables
+
+# After that run a simple linear model and a logistic regression model
+# Then, ren a stepwise model to find the best regresson model
+
+## From there, show charts for regression model.
+# show charts residuals vs. fitted, normal Q-Q,
+# scale-location, and residuals vs. leverage
